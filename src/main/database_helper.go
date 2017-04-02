5b502548495bac5881c1a219c15201f7d071d21e
andreyocheretyanyi <andrey.ocheretyanyi@gmail.com>
2017-04-02 14:45:49 +0300

    add gitignore

--- src/main/database_helper.go
+++ src/main/database_helper.go
@@ -9,10 +9,9 @@ import (
 	"fmt"
 )
 
-const db_url  = "postgres://lyxswjipntxtho:c9bbfdaa77e45ca80ef725d482571153c5f73049b53b81323bf4b44f92a95087@ec2-54-228-235-185.eu-west-1.compute.amazonaws.com:5432/d9eavq5joinmbt"
 
 func open_db() {
-	db, err := sql.Open("postgres", os.Getenv(db_url))
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	fmt.Print(db)
 	if err != nil {
 		log.Fatal(err)
